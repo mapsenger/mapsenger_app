0a5956292da318e111c17b05d146b85352b09689
jnep1009 <junep1009@gmail.com>
2016-12-22 04:13:37 +0700

    add MAPVIEW

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,3 +1,16 @@
 /**
  * Created by JNEP on 12/22/16.
  */
+import MapGL from 'react-map-gl';
+import * as React from 'react';
+export default class MapView extends React.Component {
+  render() {
+    return (<MapGL
+      width={700}
+      height={450}
+      latitude={37.78}
+      longitude={-122.45}
+      zoom={11}
+    />);
+  }
+}
